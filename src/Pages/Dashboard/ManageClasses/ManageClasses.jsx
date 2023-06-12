fd7537cf7bd3236ee228d49dfa831aba1e2a9d9f
23Mubasshir <23mubasshir@gmail.com>
2023-06-12 11:33:25 +0600

    Readme file added

--- src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,23 +1,77 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-// import { Link } from "react-router-dom";
-
+import Swal from "sweetalert2";
+// import { useState } from "react";
 
 const ManageClasses = () => {
-    const [axiosSecure] = useAxiosSecure();
+  // const [feedback, setFeedback] = useState("");
+  const [axiosSecure] = useAxiosSecure();
   const { data: classes = [], refetch } = useQuery(["classes"], async () => {
-    const res = await axiosSecure.get("/classes");
+    const res = await axiosSecure.get("/AllClasses");
     return res.data;
   });
-  const handleApprove = (user) =>{
-    console.log(user)
-  }
-  const handleDeny = (user) =>{
-    console.log(user)
-  }
-  const handleFeedback = (user) =>{
-    console.log(user)
-  }
+  const handleApprove = (item) => {
+    fetch(`http://localhost:5000/classApproved/admin/${item._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `This Class is Approved !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleDeny = (item) => {
+    fetch(`http://localhost:5000/classDenied/admin/${item._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `This Class is Denied !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+  const handleFeedback = (item) => {
+    console.log(item);
+    Swal.fire({
+      title: "Feedback",
+      text: "Write Your Feedback",
+      input: "text",
+      showCancelButton: true,
+    }).then((result) => {
+        const feedback=result.value;
+        const updatedFeedback = {feedback};
+        console.log(updatedFeedback)
+        fetch(`http://localhost:5000/update-feedback/${item._id}`, {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(updatedFeedback),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+    });
+  };
 
   return (
     <div className="w-full">
@@ -59,23 +113,46 @@ const ManageClasses = () => {
                 <td className="text-center">{item?.instructor_name}</td>
                 <td>{item?.instructor_email}</td>
                 <td>{item?.available_seats}</td>
+                <td>$ {item?.price}</td>
+                <td>{item?.role}</td>
                 <td>
-                $ {item?.price}
-                </td>
-                <td>
-                {item?.role}
-                </td>
-                <td>
-                <div className="flex flex-col gap-1" >
-                {item.role == "approved" || item.role == "denied" ? <button disabled className="btn btn-xs btn-success disabled">Approve</button> : <button onClick={handleApprove} className="btn btn-xs btn-success">Approve</button> }
+                  <div className="flex flex-col gap-1">
+                    {item.role == "approved" || item.role == "denied" ? (
+                      <button
+                        disabled
+                        className="btn btn-xs btn-success disabled"
+                      >
+                        Approve
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleApprove(item)}
+                        className="btn btn-xs btn-success"
+                      >
+                        Approve
+                      </button>
+                    )}
 
-                {(item.role == "approved") || (item.role == "denied") ? <button disabled className="btn btn-xs disabled">Deny</button> : <button onClick={handleDeny} className="btn btn-xs btn-error">Deny</button> }
-                
-                
-                <button onClick={handleFeedback} className="btn btn-xs btn-info">Feedback</button>
+                    {item.role == "approved" || item.role == "denied" ? (
+                      <button disabled className="btn btn-xs disabled">
+                        Deny
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleDeny(item)}
+                        className="btn btn-xs btn-error"
+                      >
+                        Deny
+                      </button>
+                    )}
 
-
-                </div>
+                    <button
+                      onClick={() => handleFeedback(item)}
+                      className="btn btn-xs btn-info"
+                    >
+                      Feedback
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
